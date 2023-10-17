d350cbb04a1a2292a063b85a28c746690325e888
Neil Davies <neil.davies@pnsol.com>
2023-10-17 11:13:46 +0100

    updated piecewise-poly code

--- workbench-support/src/DeltaQ/Workbench/Common.hs
+++ workbench-support/src/DeltaQ/Workbench/Common.hs
@@ -1,3 +1,37 @@
 module DeltaQ.Workbench.Common
-  ()
+  ( module WB
+  , plotCDFs
+  , plotPMFs
+  )
 where
+import Graphics.Rendering.Chart.Easy as WB hiding (close)
+import IHaskell.Display as WB
+import IHaskell.Display.Charts as WB ()
+
+import DeltaQ.Workbench.Piecewise (DeltaQ, displayCDF, displayPMF)
+
+plotCDFs :: Int               -- ^ Sample points for graphing
+         -> String            -- ^ plot title
+         -> [(String, DeltaQ Double)] -- ^ list of named ∆Q's
+         -> Renderable ()
+plotCDFs n t dqs =
+  toRenderable $ do
+    layout_title .= t
+    layout_x_axis . laxis_title .= "Delay (s)"
+    layout_y_axis . laxis_title .= "Probability Mass"
+    mapM (\(a,b) -> plot $ line a [genSamples n b]) dqs
+   where
+     genSamples = displayCDF
+
+plotPMFs :: Int               -- ^ Sample points for graphing
+         -> String            -- ^ plot title
+         -> [(String, DeltaQ Double)] -- ^ list of named ∆Q's
+         -> Renderable ()
+plotPMFs n t dqs =
+  toRenderable $ do
+    layout_title .= t
+    layout_x_axis . laxis_title .= "Delay (s)"
+    layout_y_axis . laxis_title .= "Probability Mass Density"
+    mapM (\(a,b) -> plot $ line a [genSamples n b]) dqs
+   where
+     genSamples = displayPMF
--- workbench-support/src/DeltaQ/Workbench/Piecewise.hs
+++ workbench-support/src/DeltaQ/Workbench/Piecewise.hs
@@ -1,14 +1,60 @@
 module DeltaQ.Workbench.Piecewise
-  ( module DeltaQ.Class
---  , module DeltaQ.PWPs
---  , module PWPs.IRVs
+  ( DeltaQ
+  , constructUniform
+  , constructDelta
+  , perfection
+  , bottom
+  , (<+>)
+--   , probChoice
+--   , firstToFinish
+--   , lastToFinish
+  , displayCDF
+  , displayPMF
+--   , constructCDF
+--   , constructLinearCDF
+  , probMass
+--  , multiWeightedChoice
   )
 where
 
-import DeltaQ.Class
--- import DeltaQ.PWPs ()
--- import PWPs.IRVs
--- import PWPs.Piecewise
--- import PWPs.PolyDeltas
--- import PWPs.SimplePolynomials
--- import PWPs.ConvolutionClasses
+import DeltaQ.Class()
+import PWPs.IRVs (IRV)
+import qualified PWPs.IRVs as P
+
+-- temporarily make probability and delay type the same
+
+type DeltaQ  t =  IRV t
+
+constructUniform :: Double -> DeltaQ Double
+constructUniform = P.constructUniform
+
+constructDelta :: Double -> DeltaQ Double
+constructDelta = P.constructDelta
+
+(<+>) :: DeltaQ Double -> DeltaQ Double -> DeltaQ Double
+(<+>) = (P.<+>)
+
+--   , probChoice
+--   , firstToFinish
+--   , lastToFinish
+
+displayCDF :: Int -> DeltaQ Double -> [(Double, Double)]
+displayCDF = P.displayCDF
+
+displayPMF :: Int -> DeltaQ Double -> [(Double, Double)]
+displayPMF  = P.displayPDF
+
+--   , constructCDF
+--   , constructLinearCDF
+--   , probMass
+
+probMass :: DeltaQ Double -> Double
+probMass = P.probMass
+
+--  , multiWeightedChoice
+
+perfection :: DeltaQ Double
+perfection = constructDelta 0
+
+bottom :: DeltaQ Double
+bottom = P.zeroPDF
